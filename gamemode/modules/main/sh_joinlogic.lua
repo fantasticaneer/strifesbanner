684484dd7dc0f851cd6f16addb370662d31e6c0a
fantasticaneer <mailspamcorp@gmail.com>
2018-03-26 01:51:06 -0500

    sets CharName to Testo instead of charid to '0'

--- gamemode/modules/main/sh_joinlogic.lua
+++ gamemode/modules/main/sh_joinlogic.lua
@@ -2,21 +2,22 @@ local function tablesetup()
     if sql.TableExists("slot1_data") == true then
         return
         end
-    sql.Query( "CREATE TABLE slot1_data( SteamID TEXT, CharID INTEGER )" )
+    sql.Query( "CREATE TABLE slot1_data( SteamID TEXT, CharName TEXT )" )
     end
     hook.Add("Initialize", "tablesetups", tablesetup)
     
-    descriptor = {"Lilly", "Bob", "Ashley", "Joe", "Rick"}
+    randomnames = {"Lilly", "Bob", "Ashley", "Joe", "Rick"}
     
     local function spawned( ply )
     print( ply:GetName().." joined the game.\n" )
-    print(sql.QueryValue("SELECT CharID FROM slot1_data WHERE SteamID = '"..ply:SteamID().."'" ))
-    if sql.QueryValue("SELECT CharID FROM slot1_data WHERE SteamID = '"..ply:SteamID().."'" ) == "0" then
+    print(sql.QueryValue("SELECT CharName FROM slot1_data WHERE SteamID = '"..ply:SteamID().."'" ))
+    if sql.QueryValue("SELECT CharName FROM slot1_data WHERE SteamID = '"..ply:SteamID().."'" ) == "testo" then
         print( ply:GetName().." has a character already.\n" )
+        print ("Character name is " ..sql.QueryValue("SELECT CharName FROM slot1_data WHERE SteamID = '"..ply:SteamID().."'" ))
         return true
     else
-        print ("Creating new CharID 0 for "..ply:SteamID().. "")
-        sql.Query( "INSERT INTO slot1_data( SteamID, CharID ) VALUES( '"..ply:SteamID().."', 0 )" )
+        print ("Creating new CharName for "..ply:SteamID().. "")
+        sql.Query( "INSERT INTO slot1_data( SteamID, CharName ) VALUES( '"..ply:SteamID().."', 'testo')") 
     end
     end
     hook.Add( "PlayerSpawn", "spawnlogic", spawned )
